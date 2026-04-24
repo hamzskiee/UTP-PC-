ac0d55468412a06299314b2865a9866e0330e490
hamzskiee <ilhamrizkyy24@gmail.com>
2026-04-25 00:27:28 +0700

    vercel

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ import os
 
 app = Flask(__name__, template_folder='.', static_folder='.', static_url_path='')
 
+app = app 
+
 def decode_image(data):
     try:
         encoded_data = data.split(',')[1]
